fc1e7973ee5c46cecb4c9e481a13c12fdd82ffe8
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-04-02 00:39:18 +0900

    실전 예제 2 - 연관관계 매핑 시작

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -16,6 +19,23 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            // 비즈니스로직 예시 코드
+
+            /* 양방향 사용시
+            Order order = new Order();  // 주문 객체 만들어서
+            order.addOrderItem(new OrderItem());  // 원하는 orderitem을 쭉쭉 넣을수 있겠죠?
+             */
+
+            // 단방향 연관관계로도 충분히 풀 수 있음!
+            Order order = new Order();
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
